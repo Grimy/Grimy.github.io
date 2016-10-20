a9c92874238e3c3663500d3fa01d4221892ec9e7
Victor Adam <victor@drawall.cc>
2016-10-21 01:51:20 +0200

    Perks: save parsing

--- perks.js
+++ perks.js
@@ -1,46 +1,62 @@
 ':' //; exec node "$0"
 
+const equipment = {
+	shield: [40, 4, 'health'],
+	dagger: [40, 2, 'attack'],
+	boots: [55, 6, 'health'],
+	mace: [80, 3, 'attack'],
+	helm: [100, 10, 'health'],
+	pole: [140, 4, 'attack'],
+	pants: [160, 14, 'health'],
+	axe: [230, 7, 'attack'],
+	guards: [275, 23, 'health'],
+	sword: [375, 9, 'attack'],
+	plate: [415, 35, 'health'],
+	arbalest: [450, 15, 'attack'],
+	gambeson: [500, 60, 'health'],
+};
+
 // Cost of the first level of each perk
 const base_cost = {
-	loot2:   100e3,
-	carp2:   100e3,
-	moti2:   50e3,
-	power2:  20e3,
-	tough2:  20e3,
-	ok:      1e6,
-	reso:    50e3,
-	coord:   150e3,
-	sipho:   100e3,
-	anti:    1000,
-	resi:    100,
-	medit:   75,
-	relent:  75,
-	carp:    25,
-	arti:    15,
-	range:   1,
-	agility: 4,
-	bait:    4,
-	trumps:  3,
-	phero:   3,
-	packrat: 3,
-	moti:    2,
-	power:   1,
-	tough:   1,
-	loot:    1,
+	Looting_II:     100e3,
+	Carpentry_II:   100e3,
+	Motivation_II:  50e3,
+	Power_II:       20e3,
+	Toughness_II:   20e3,
+	Overkill:       1e6,
+	Resourceful:    50e3,
+	Coordinated:    150e3,
+	Siphonology:    100e3,
+	Anticipation:   1000,
+	Resilience:     100,
+	Meditation:     75,
+	Relentlessness: 75,
+	Carpentry:      25,
+	Artisanistry:   15,
+	Range:          1,
+	Agility:        4,
+	Bait:           4,
+	Trumps:         3,
+	Pheromones:     3,
+	Packrat:        3,
+	Motivation:     2,
+	Power:          1,
+	Toughness:      1,
+	Looting:        1,
 };
 
 // Cost increment, in percentage of the base cost, for tier II perks
-const increment = {tough2: 2.5, power2: 2.5, moti2: 2, carp2: 10, loot2: 10};
+const increment = {Toughness_II: 2.5, Power_II: 2.5, Motivation_II: 2, Carpentry_II: 10, Looting_II: 10};
 
 // Maximum level, for the perks that have one
-const cap = {range: 10, agility: 20, relent: 10, medit: 7, anti: 10, sipho: 3, ok: 30};
+const cap = {Range: 10, Agility: 20, Relentlessness: 10, Meditation: 7, Anticipation: 10, Siphonology: 3, Overkill: 30};
 
 const perks = Object.keys(base_cost);
 
 function optimize(params) {
 	"use strict";
 
-	var {he_left, zone, last_unlock, weight, mod} = params;
+	var {he_left, zone, unlocks, weight, climb, mod} = params;
 	if (!(he_left <= 1e16))
 		return;
 
@@ -53,17 +69,9 @@ function optimize(params) {
 	// Total bonus from a compounding perk. `x` is the percentage from each level.
 	const mult = (perk, x) => pow(1 + x / 100, level[perk]);
 
-	// Return a function that returns the total bonus granted by equipment for a given stat.
-	// cost: total cost of tier I equipments for the stat
-	// value: total bonus granted by relevant tier I equipments
-	// exp: 13 for attack, 14 for health (defined in game.global.prestige)
-	// TODO handle foo-climbing
-	function precompute_equipment_ratios(cost, value, exp) {
-		exp /= 0.85;
-		cost *= 1.069;
-		value *= pow(1.19, 1 - 0.3 * exp);
-		exp /= (zone < 60 ? 57 : 53) * log(1.069) / log(1.19);
-		return () => pow(income() * trimps() / (cost * mult('arti', -5)), exp) * value;
+	function tiers(stat) {
+		let {cost, value, exp} = equip_total[stat];
+		return pow(income() * trimps() / (cost * mult('Artisanistry', -5)), exp) * value;
 	}
 
 	const corruption_start = 181;
@@ -97,32 +105,32 @@ function optimize(params) {
 
 	// Max population
 	function trimps() {
-		var carp = mult('carp', 10) * add('carp2', 0.25);
-		var bonus = mod.housing + log(income() / base_income * carp / mult('reso', -5));
-		var territory = add('trumps', 20) * zone;
-		return 10 * (base_housing * bonus + territory) * carp * imp.taunt;
+		var Carpentry = mult('Carpentry', 10) * add('Carpentry_II', 0.25);
+		var bonus = mod.housing + log(income() / base_income * Carpentry / mult('Resourceful', -5));
+		var territory = add('Trumps', 20) * zone;
+		return 10 * (base_housing * bonus + territory) * Carpentry * imp.taunt;
 	}
 
 	// Number of ticks it takes to one-shot an enemy.
 	function ticks() {
-		return 2 + floor(10 * mult('agility', -5));
+		return 2 + floor(10 * mult('Agility', -5));
 	}
 
 	// Number of buildings of a given kind that can be built with the current income.
 	// cost: base cost of the buildings
 	// exp: cost increase for each new level of the building
 	function building(cost, exp) {
-		cost *= 4 * mult('reso', -5);
+		cost *= 4 * mult('Resourceful', -5);
 		return log(income() * trimps() * (exp - 1) / cost + 1) / log(exp);
 	}
 
-	const moti = () => add('moti', 5) * add('moti2', 1);
-	const looting = () => add('loot', 5) * add('loot2', 0.25);
+	const Motivation = () => add('Motivation', 5) * add('Motivation_II', 1);
+	const looting = () => add('Looting', 5) * add('Looting_II', 0.25);
 
 	// Total resource gain per second
 	function income() {
-		var storage = mod.storage * mult('reso', -5) / add('packrat', 20);
-		var prod = moti() * add('medit', 1) * (1 + mod.turkimp / 2);
+		var storage = mod.storage * mult('Resourceful', -5) / add('Packrat', 20);
+		var prod = Motivation() * add('Meditation', 1) * (1 + mod.turkimp / 2);
 		var lmod = looting() * imp.magn * mod.loot / ticks();
 		var loot = base_loot * lmod * (1 + .166 * mod.turkimp);
 		var chronojest = mod.chronojest * 0.75 * prod * lmod;
@@ -133,8 +141,8 @@ function optimize(params) {
 	function breed() {
 		var nurseries = pow(1.01, building(2e6, 1.06));
 		var potency = pow(1.1, floor(zone / 5));
-		var bait = weight.breed * add('bait', 100) * 10 * mod.breed_timer / trimps();
-		return 0.00085 * nurseries * potency * add('phero', 10) * imp.ven + bait;
+		var traps = weight.breed * add('Bait', 100) * 10 * mod.breed_timer / trimps();
+		return 0.00085 * nurseries * potency * add('Pheromones', 10) * imp.ven + traps;
 	}
 
 	function group_size(ratio) {
@@ -146,18 +154,18 @@ function optimize(params) {
 
 	// Theoretical fighting group size (actual size is lower because of Coordinated) 
 	function soldiers() {
-		var ratio = 1 + .25 * pow(.98, level['coord']);
+		var ratio = 1 + .25 * pow(.98, level['Coordinated']);
 		var coords = log(trimps() / 3 / group_size(ratio)) / log(ratio);
 		return group_size(1.25) * Math.pow(1.25, min(zone - 1, coords));
 	}
 
 	// Total attack
 	function attack() {
-		var power = add('power', 5) * add('power2', 1) * add('range', 1);
-		var crits = add('relent', 5 * add('relent', 30));
-		var sipho = pow(1 + level['sipho'], 0.1);
-		var anti = add('anti', 2 * mod.breed_timer);
-		return soldiers() * weapons() * power * crits * sipho * anti;
+		var power = add('Power', 5) * add('Power_II', 1) * add('Range', 1);
+		var crits = add('Relentlessness', 5 * add('Relentlessness', 30));
+		var sipho = pow(1 + level['Siphonology'], 0.1);
+		var anti = add('Anticipation', 2 * mod.breed_timer);
+		return soldiers() * tiers('attack') * power * crits * sipho * anti;
 	}
 
 	// Block per imp
@@ -170,20 +178,20 @@ function optimize(params) {
 
 	// Total survivability (accounts for health and block)
 	function health() {
-		var health = armors() * add('tough', 5) * mult('resi', 10) * add('tough2', 1);
+		var health = tiers('health') * add('Toughness', 5) * mult('Resilience', 10) * add('Toughness_II', 1);
 		if (zone >= 70 && weight.breed == 0) {
 			var target_speed = pow(6, 0.1 / mod.breed_timer);
 			var geneticists = log(breed() / target_speed) / log(1.02);
 			health *= pow(1.01, geneticists);
 		}
-		return soldiers() * (health + 4 * min(block(), health));
+		return soldiers() * (health + 0 * min(block(), health));
 	}
 
 	function helium() {
 		return base_helium * looting() + 45;
 	}
 
-	const overkill = () => attack() * add('ok', 60);
+	const overkill = () => add('Overkill', 60);
 
 	const stats = { helium, attack, health, overkill, breed }
 
@@ -193,7 +201,7 @@ function optimize(params) {
 		for (var i in weight)
 			if (weight[i] != 0)
 				result += weight[i] * log(stats[i]());
-		return result / mult('agility', -0.1);
+		return result / mult('Agility', -0.1);
 	}
 
 	function best_perk() {
@@ -201,7 +209,7 @@ function optimize(params) {
 		var max = 0;
 		var baseline = score();
 
-		for (var perk of perks.slice(perks.indexOf(last_unlock))) {
+		for (var perk of unlocks) {
 			if (level[perk] === cap[perk] || cost(perk) > he_left)
 				continue;
 
@@ -241,8 +249,6 @@ function optimize(params) {
 	var scientist_done = zone > 130;
 	var slow = zone > 130;
 	var frugal_done = zone > 100 ? 1.28 : 1.2;
-	var armors = precompute_equipment_ratios(slow ? 1545 : 1045, slow ? 152 : 92, 14);
-	var weapons = precompute_equipment_ratios(slow ? 1315 : 865, slow ? 40 : 25, 13);
 	var books = pow(1.25, zone) * pow(frugal_done, max(zone - 59, 0));
 	var gigas = min(zone - 60, zone / 2 - 25, zone / 3 - 12, zone / 5, zone / 10 + 17);
 	var base_housing = pow(1.25, min(zone / 2, 30) + mod.giga * max(0, 0|gigas));
@@ -252,10 +258,31 @@ function optimize(params) {
 	var base_income = income();
 	var base_helium = run_helium(zone);
 
+	// Precompute equipment ratios
+	const equip_total = {
+		attack: {cost: 0, value: 0, exp: 13},
+		health: {cost: 0, value: 0, exp: 14},
+	}
+
+	for (let piece in equipment) {
+		let [cost, value, stat] = equipment[piece];
+		equip_total[stat].cost += cost;
+		equip_total[stat].value += value;
+		if (piece == climb)
+			break;
+	}
+
+	for (let stat in equip_total) {
+		equip_total[stat].exp /= 0.85;
+		equip_total[stat].cost *= 1.069;
+		equip_total[stat].value *= pow(1.19, 1 - 0.3 * equip_total[stat].exp);
+		equip_total[stat].exp /= (zone < 60 ? 57 : 53) * log(1.069) / log(1.19);
+	}
+
 	// Main loop
 	var free = he_left / 1000;
-	var shitty = {bait: true, packrat: true, trumps: true};
-	for (let best = 'loot'; best; best = best_perk()) {
+	var shitty = {Bait: true, Packrat: true, Trumps: true};
+	for (let best = 'Looting'; best; best = best_perk()) {
 		var spent = 0;
 		do {
 			spent += cost(best);
@@ -266,11 +293,11 @@ function optimize(params) {
 
 	// Debug stuff
 	var potential_helium = run_helium(zone + 10);
+	console.log(equip_total);
 	console.log('Suggested looting weight:', log(1024) / log(potential_helium / base_helium));
 
-	compare('coord', 'carp');
-	compare('arti', 'reso');
-	compare('loot', 'moti');
+	console.log(tiers('health'));
+	compare('Power', 'Artisanistry');
 	console.log('Max trimps', trimps());
 
 	return level;
@@ -281,8 +308,8 @@ if (typeof window === 'undefined') {
 	console.log(optimize({
 		he_left: 1e15,
 		zone: 350,
-		last_unlock: 'loot2',
-		weight: {helium: 70, attack: 3, breed: 0, health: 1, overkill: 1},
+		weight: {helium: 70, attack: 1, breed: 0, health: 1, overkill: 1},
+		climb: 'plate',
 		mod: {
 			storage: 0.125,
 			whip: true,
@@ -290,7 +317,7 @@ if (typeof window === 'undefined') {
 			taunt: true,
 			ven: true,
 			chronojest: 5,
-			loot: 1,
+			Looting: 1,
 			turkimp: .5,
 			breed_timer: 30,
 			giga: 1,
