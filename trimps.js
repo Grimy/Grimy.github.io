9c5956eb05651568d0cae8df25cb0c4eee82bc66
Victor Adam <victor.adam@engie.com>
2017-09-29 14:17:37 +0200

    perky, zFarm: support for non-standard notations
    refs #11

--- trimps.js
+++ trimps.js
@@ -1,3 +1,4 @@
+// trimps.js: some code common to Perky and zFarm
 
 function $(str) { return document.querySelector(str); }
 
@@ -7,22 +8,50 @@ function switch_theme() {
 	localStorage.setItem('dark', light ? '' : '1');
 }
 
+function handle_paste(ev) {
+	let save_string = ev.clipboardData.getData("text/plain").replace(/\s/g, '');
+	let game = JSON.parse(LZString.decompressFromBase64(save_string));
+	$('#load-failed').style.display = game ? 'none' : 'block';
+	if (game) {
+		$('#bad-version').style.display = game.global.version <= 4.5 ? 'none' : 'block';
+		localStorage.setItem('notation', game.options.menu.standardNotation.enabled);
+		read_save(game);
+		main();
+	}
+}
+
 const suffixes = [
 	'K', 'M', 'B', 'T', 'Qa', 'Qi', 'Sx', 'Sp', 'Oc', 'No', 'Dc',
 	'Ud', 'Dd', 'Td', 'Qad', 'Qid', 'Sxd', 'Spd', 'Od', 'Nd', 'V',
 	'Uv', 'Dv', 'Tv', 'Qav', 'Qiv', 'Sxv', 'Spv', 'Ov', 'Nv', 'Tt',
 ];
 
+const alphabet = "\u200babcdefghijklmnopqrstuvwxyz";
+
 function prettify(number) {
 	if (number < 10000)
 		return Math.round(number);
-	let unit = 0;
-	while (number > 1000) {
+
+	if (localStorage.getItem('notation') == '0') // scientific
+		return number.toExponential(2).replace('+', '');
+
+	let unit = -1;
+	while (number >= 999.5) {
 		number /= 1000;
 		++unit;
 	}
-	let digits = number == Math.floor(number) ? 0 : (number < 10) + (number < 100);
-	return number.toFixed(digits) + (suffixes[unit - 1] || `E${3 * unit}`);
+
+	let precision = number == Math.floor(number) ? 0 : (number < 10) + (number < 100);
+	number = number.toFixed(precision);
+
+	if (localStorage.getItem('notation') == 3) // alphabetic
+		return number + alphabet[Math.floor(unit / 26)] + alphabet[unit % 26 + 1];
+
+	if (localStorage.getItem('notation') == 2 || unit >= suffixes.length) // engineering
+		return number + 'e' + (3 * unit + 3);
+
+	// standard
+	return number + suffixes[unit];
 }
 
 function parse_suffixes(str) {
