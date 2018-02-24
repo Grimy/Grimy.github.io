f6a5186580aaa7a8d237e8c7bf98a7975d795dd6
Victor Adam <victor@drawall.cc>
2018-02-24 12:39:18 +0100

    common: fix hybrid parsing (closes #40)

--- trimps.js
+++ trimps.js
@@ -93,7 +93,7 @@ const notations = [
 	' aa ab ac ad ae af ag ah ai aj ak al am an ao ap aq ar as at au av aw ax ay az' +
 	' ba bb bc bd be bf bg bh bi bj bk bl bm bn bo bp bq br bs bt bu bv bw bx by bz' +
 	' ca cb cc cd ce cf cg ch ci cj ck cl cm cn co cp cq cr cs ct cu cv cw cx').split(' '),
-	'KMBTQaQiSxSpOcNoDcUdDdTdQadQidSxdSpdOdNdVUvDvTvQavQivSxvSpvOvNvTt'.split(/(?=[A-Z])/),
+	'KMBTQaQiSxSpOcNoDcUdDdTdQadQidSxdSpdOdNdVUvDvTvQavQivSxvSpvOvNvTg'.split(/(?=[A-Z])/),
 ];
 
 function prettify(number) {
