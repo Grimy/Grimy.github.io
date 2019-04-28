50aef99fc23a6fdbc50b8d0de3d74bb36c1ca534
Victor Adam <victor@drawall.cc>
2019-04-28 12:27:09 +0200

    common: support logarithmic notation

--- src/trimps.ts
+++ src/trimps.ts
@@ -72,6 +72,7 @@ const notations = [
 	' ba bb bc bd be bf bg bh bi bj bk bl bm bn bo bp bq br bs bt bu bv bw bx by bz' +
 	' ca cb cc cd ce cf cg ch ci cj ck cl cm cn co cp cq cr cs ct cu cv cw cx').split(' '),
 	'KMBTQaQiSxSpOcNoDcUdDdTdQadQidSxdSpdOdNdVUvDvTvQavQivSxvSpvOvNvTg'.split(/(?=[A-Z])/),
+	[],
 ];
 
 function prettify(number: number): string {
