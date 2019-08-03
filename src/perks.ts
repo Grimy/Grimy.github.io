6cb469252cadafe914f90655d38f9551874dcc08
Victor Adam <victor@drawall.cc>
2019-08-03 21:41:04 +0200

    perky: add 5.0 perks

--- src/perks.ts
+++ src/perks.ts
@@ -366,6 +366,11 @@ function parse_perks(fixed: string, unlocks: string) {
 	const mult = (x: number) => (level: number) => pow(1 + x * 0.01, level);
 
 	let perks: {[key: string]: Perk} = {
+		Tenacity:       new Perk(1e308, 0,    add(0)),
+		Criticality:    new Perk(1e308, 0,    add(0)),
+		Equality:       new Perk(1e308, 0,    add(0)),
+		Prismal:        new Perk(1e308, 0,    add(0)),
+
 		Looting_II:     new Perk(100e3, 10e3, add(0.25)),
 		Carpentry_II:   new Perk(100e3, 10e3, add(0.25)),
 		Motivation_II:  new Perk(50e3,  1e3,  add(1)),
@@ -425,6 +430,11 @@ function parse_perks(fixed: string, unlocks: string) {
 		if (m[2] != '<')
 			perks[matches[0]].min_level = level;
 	}
+
+	perks.Tenacity.locked = true;
+	perks.Criticality.locked = true;
+	perks.Equality.locked = true;
+	perks.Prismal.locked = true;
 
 	return perks;
 }
